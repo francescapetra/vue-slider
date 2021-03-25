b4d6284fac2e40ed2c7aa94f340985fdc38f6511
francescapetra <francescalaurapetra@gmail.com>
2021-03-25 15:27:04 +0100

    aggiunte img prova

--- script/script.js
+++ script/script.js
@@ -2,11 +2,16 @@ var app = new Vue(
   {
     el:"#root",
     data:{
-      message: "",
-      miaClasse : "red",
-    },
+      images: [
+          "https://www.sephora.it/dw/image/v2/BCVW_PRD/on/demandware.static/-/Library-Sites-SephoraV2/default/dwd7c6f34c/LP/xmas-logo/sepho-logo-anim-desk_v2.gif?q=75",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp7BTMStct0Bdjt2m6-w-TLjQAJdrtnILdgg&usqp=CAU",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNxTlOFNycETFtOARjFF6itBflWb2bviIOug&usqp=CAU",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa2nWeSDVwz8XqvIEVjqqECluCOmwVkcwwrQ&usqp=CAU"
+      ],
+      imageIndex : 0,
     methods:{
 
+    }
     }
   }
 );
