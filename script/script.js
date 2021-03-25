b0123f12c2201ce21f5f5d655d0ea948b418e488
francescapetra <francescalaurapetra@gmail.com>
2021-03-25 16:44:45 +0100

    bullet error

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ var app = new Vue(
     el:"#root",
     data:{
       images: [
-          "https://www.sephora.it/dw/image/v2/BCVW_PRD/on/demandware.static/-/Library-Sites-SephoraV2/default/dwd7c6f34c/LP/xmas-logo/sepho-logo-anim-desk_v2.gif?q=75",
+          "https://www.aprireinfranchising.it/useruploads/images/franchisinglist/sephora-franchising-list-aprire.jpg",
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp7BTMStct0Bdjt2m6-w-TLjQAJdrtnILdgg&usqp=CAU",
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNxTlOFNycETFtOARjFF6itBflWb2bviIOug&usqp=CAU",
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa2nWeSDVwz8XqvIEVjqqECluCOmwVkcwwrQ&usqp=CAU"
@@ -24,6 +24,10 @@ var app = new Vue(
         if (this.imagesIndex < 0) {
           this.imagesIndex = this.images.length-1;
         }
+      },
+      bulletImage: function(i){
+        this.imagesIndex = i;
+        console.log(this);
       }
     }
   }
